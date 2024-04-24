198026bce235635f6bb592d1323bda5716c5826e
NatnaelWale <nhattyjeje@gmail.com>
2024-04-24 15:43:20 -0400

    switch and if statement exercise added

---
+++ demo-3-18.js
@@ -0,0 +1,60 @@
+"use strict";
+
+let dayNum = 0;
+
+let dayName;
+
+//  Here is one way of doing this case with an if statement
+
+// if (dayNum == 0) {
+//     dayName = "Sunday";
+// }
+// else if ( dayNum == 1) {
+//     dayName = "Monday";
+// }
+// else if (dayNum == 2) {
+//     dayName = "Tuesday";
+// }
+// else if ( dayNum == 3) {
+//     dayName = "Wednesday";
+// }
+// else if (dayNum == 4) {
+//     dayName = "Thursday";
+// }
+// else if ( dayNum == 5) {
+//     dayName = "Friday";
+// }
+// else if (dayNum == 6) {
+//     dayName = "Saturday";
+// }
+// console.log(dayName)
+
+
+// Here is how to do the same thing with switch statement
+
+
+// switch (dayNum) {
+//     case 0:
+//         dayName = "Sunday";
+//         break;
+//     case 1:
+//         dayName = "Monday";
+//         break
+//     case 2:
+//         dayName = "Tuesday";
+//         break
+//     case 3:
+//         dayName = "Wednesday";
+//         break;
+//     case 4:
+//         dayName = "Thursday";
+//         break
+//     case 5:
+//         dayName = "Friday";
+//         break;
+//     case 6:
+//         dayName = "Saturday";
+//         break
+// }
+
+// console.log(dayName);
